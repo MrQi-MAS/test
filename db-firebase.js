11efb4ce5719ac7f4f9380269bff331227bdc532
MrQi-MAS <bugfizzz@gmail.com>
2026-06-13 01:29:51 +0700

    retest home

--- db-firebase.js
+++ db-firebase.js
@@ -1,4 +1,3 @@
-
 // ════════════════════════════════════════
 // db-firebase.js
 // เชื่อมต่อกับ Firebase Realtime Database
@@ -25,7 +24,8 @@
 
 const firebaseConfig = {
     // Import the functions you need from the SDKs you need
-  
+  import { initializeApp } from "firebase/app";
+  import { getAnalytics } from "firebase/analytics";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -43,7 +43,8 @@ const firebaseConfig = {
   };
 
   // Initialize Firebase
-
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 };
 
 firebase.initializeApp(firebaseConfig);
