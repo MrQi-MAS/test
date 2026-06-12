4e508e1d6e6b541852b87b97bc413f9bd3fb624b
MrQi-MAS <bugfizzz@gmail.com>
2026-06-13 01:38:15 +0700

    Updatea db-firebase.js

--- db-firebase.js
+++ db-firebase.js
@@ -23,7 +23,7 @@
 // ════════════════════════════════════════
 
 const firebaseConfig = {
-    // Import the functions you need from the SDKs you need
+  // Import the functions you need from the SDKs you need
   import { initializeApp } from "firebase/app";
   import { getAnalytics } from "firebase/analytics";
   // TODO: Add SDKs for Firebase products that you want to use
